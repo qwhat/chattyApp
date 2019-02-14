df5381e2b351e1cec148cc78bd533f6b4e025865
qwhat <justinmao@live.ca>
2019-02-14 20:36:33 +0000

    set up initial WebSockeet Server

--- src/App.jsx
+++ src/App.jsx
@@ -8,53 +8,25 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-        currentUser: "Anonymous",
-         messages: [
-      {
-        id: 1,
-        type: "incomingMessage",
-        content: "I won't be impressed with technology until I can download food.",
-        username: "Anonymous1"
-      },
-      {
-        id: 2,
-        type: "incomingNotification",
-        content: "Anonymous1 changed their name to nomnom",
-      },
-      {
-        id: 3,
-        type: "incomingMessage",
-        content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
-        username: "Anonymous2"
-      },
-      {
-        id: 4,
-        type: "incomingMessage",
-        content: "...",
-        username: "nomnom"
-      },
-      {
-        id: 5,
-        type: "incomingMessage",
-        content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
-        username: "Anonymous2"
-      },
-      {
-        id: 6,
-        type: "incomingMessage",
-        content: "This isn't funny. You're not funny",
-        username: "nomnom"
-      },
-      {
-        id: 7,
-        type: "incomingNotification",
-        content: "Anonymous2 changed their name to NotFunny",
-      },
-    ]}
+      currentUser: "Anonymous",
+      messages: []
+    }
   }
 
   componentDidMount() {
-    console.log("componentDidMount <App />");
+    const url = 'ws://0.0.0.0:3001/'
+    this.socket = new WebSocket(url);
+    this.socket.onopen = event => {
+      console.log("Connected to socket server");
+      this.socket.send(JSON.stringify(this.state.messages))
+
+    }
+    this.socket.onmessage = message => {
+      const serverMessage = JSON.parse(message.data);
+      console.log(serverMessage);
+    }
+
+
     setTimeout(() => {
       console.log("Simulating incoming message");
       // Add a new message to the list of messages in the data store
@@ -72,6 +44,7 @@ class App extends Component {
     if(event.key === 'Enter'){
       const newMessage = {id:(this.state.messages.length +1), type:"incomingMessage", username: this.state.currentUser, content:(event.target.value)}
       this.setState({messages:[...this.state.messages, newMessage]})
+                  this.socket.send(JSON.stringify(newMessage))
     }
   }
 
