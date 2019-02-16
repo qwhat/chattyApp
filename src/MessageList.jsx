990e313d93cd8f653a9e1b3247202823275fcbdc
qwhat <justinmao@live.ca>
2019-02-16 06:31:01 +0000

    comment the code in messagelist to make it easier to read

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Message} from './Message.jsx'
 
+/* functional component that applies a function to the message list */
 export function MessageList(props) {
     const eachMessage = props.messages.map((message)=><Message message={message} key={message.id}/>)
     return (
